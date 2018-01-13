32ac51d79652e8acb47345e7abfc285bbf9d32bf
Jason Justice <squallseed31@gmail.com>
2018-01-13 14:39:31 -0800

    Proof-of-concept able to connect to Discord/Google

--- MinusFifty/Program.cs
+++ MinusFifty/Program.cs
@@ -1,15 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System;
 using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using JsonConfig;
 
 namespace MinusFifty
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+
+            _commands = new CommandService();
+
+            _client.Log += Log;
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            await InstallCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, Config.Global.DiscordToken);
+            await _client.StartAsync();
+
+            Persistence.GoogleSheetsHelper.testSheets();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null)
+                return;
+
+            int argPos = 0;
+            if (!(message.HasCharPrefix((char)Config.Global.CommandPrefix, ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)))
+                return;
+
+            var context = new SocketCommandContext(_client, message);
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
         }
     }
 }
